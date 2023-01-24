c46e8476f236f3df3623b71547c29a81a64111c5
Jean <acalto2@gmail.com>
2023-01-24 21:13:25 +0100

    initial

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Text('This is My Homepage',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ))),
+      ),
+    ),
+  );
+}
+// what this code does? 
