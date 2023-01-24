77385d960c9e9f728ab8768b34ee979ba2e58bf6
Jean <acalto2@gmail.com>
2023-01-24 22:35:24 +0100

    15

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,77 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(
-    MaterialApp(
+void main() => runApp(myFirstApp());
+
+class myFirstApp extends StatefulWidget {
+  @override
+  State<myFirstApp> createState() => _myFirstAppState();
+}
+
+class _myFirstAppState extends State<myFirstApp> {
+  late bool _loading;
+  late double _progressValue;
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('My First app'),
           centerTitle: true,
         ),
         body: Center(
-            child: Text('This is My Homepage',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ))),
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      LinearProgressIndicator(value: _progressValue),
+                      Text(
+                        '${(_progressValue * 100).round()} %',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  )
+                : Text(
+                    'Press button to download',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: Icon(Icons.file_download),
+        ),
       ),
-    ),
-  );
+    );
+  }
+
+  void _updateProgress() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.1;
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          t.cancel();
+          _loading = false;
+          _progressValue = 0;
+        }
+      });
+    });
+  }
 }
-// what this code does? 
