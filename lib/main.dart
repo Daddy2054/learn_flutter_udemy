5e27eb911f4f9367108fada91f2bd292999edd86
Jean <acalto2@gmail.com>
2023-01-25 23:33:53 +0100

    23

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
-void main() {
-  runApp(MyApp());
+void main(List<String> args) {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: MyHomePage(title: 'Counter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 50;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Tap "-" to decrement',
-              style: TextStyle(color: Colors.white),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Building layouts'),
+          centerTitle: true,
+        ),
+        body: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.red,
+                child: Text('1'),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Ink(
-                  decoration: ShapeDecoration(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            topLeft: Radius.circular(10))),
-                  ),
-                  child: IconButton(
-                      onPressed: _decrementCounter,
-                      color: Colors.indigo,
-                      icon: Icon(Icons.remove)),
-                ),
-                Container(
-                    decoration: BoxDecoration(color: Colors.white),
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.white)),
-                        onPressed: null,
-                        child: Text(
-                          '$_counter',
-                          style: TextStyle(fontSize: 24),
-                        ))),
-                Ink(
-                    decoration: ShapeDecoration(
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              bottomRight: Radius.circular(10),
-                              topRight: Radius.circular(10))),
-                    ),
-                    child: IconButton(
-                        onPressed: _incrementCounter, icon: Icon(Icons.add))),
-              ],
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.green,
+                child: Text('2'),
+              ),
             ),
-            Text(
-              'Tap "+" to increment',
-              style: TextStyle(color: Colors.white),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.blue,
+                child: Text('3'),
+              ),
             ),
           ],
         ),
       ),
-      backgroundColor: Colors.indigo[300],
     );
   }
 }
