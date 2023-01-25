3d7ee55179c1f822d8ab11ce1ae4f51ba147cfa1
Jean <acalto2@gmail.com>
2023-01-25 23:19:37 +0100

    17

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,109 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:ThemeData(fontFamily: 'IndieFlower'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Adding Assets'),
-        ),
-        body: Center(
-            child: Stack(
-          fit: StackFit.expand,
-          children: [
-            Image(
-              image: AssetImage('assets/images/bg.jpg'),
-            ),
-            Image.asset('assets/icons/icon.png'),
-            Positioned(
-              top:16,left:115,
-              child: Text(
-                'My custom font',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-//                  fontFamily: 'IndieFlower',
-                ),
-              ),
-            )
-          ],
-        )),
+      title: 'Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
       ),
+      home: MyHomePage(title: 'Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 50;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Tap "-" to decrement',
+              style: TextStyle(color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Ink(
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            topLeft: Radius.circular(10))),
+                  ),
+                  child: IconButton(
+                      onPressed: _decrementCounter,
+                      color: Colors.indigo,
+                      icon: Icon(Icons.remove)),
+                ),
+                Container(
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white)),
+                        onPressed: null,
+                        child: Text(
+                          '$_counter',
+                          style: TextStyle(fontSize: 24),
+                        ))),
+                Ink(
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10),
+                              topRight: Radius.circular(10))),
+                    ),
+                    child: IconButton(
+                        onPressed: _incrementCounter, icon: Icon(Icons.add))),
+              ],
+            ),
+            Text(
+              'Tap "+" to increment',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.indigo[300],
     );
   }
 }
