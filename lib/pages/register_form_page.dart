6c0bf2809fb051adebffac1e5a45485bb35d4cc5
Jean <acalto2@gmail.com>
2023-01-29 21:52:38 +0100

    47

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -8,18 +8,61 @@ class RegisterFormPage extends StatefulWidget {
 }
 
 class _RegisterFormPageState extends State<RegisterFormPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Register Form'),
-        centerTitle: true,
-      ),
-      body: Form(child: ListView(
-        padding: EdgeInsets.all(16),
-      ),)
-      );
-    
+        appBar: AppBar(
+          title: Text('Register Form'),
+          centerTitle: true,
+        ),
+        body: Form(
+          child: ListView(padding: EdgeInsets.all(16), children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Full name *',
+                hintText: 'Enter your name',
+              ),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Phone Number *',
+                hintText: 'Enter your email',
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email *',
+                hintText: 'Enter your email',
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Life Story',
+                hintText: 'Enter your story',
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password *',
+                hintText: 'Enter your password',
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Confirm Password *',
+                hintText: 'Confirm your password',
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Submit Form', style: TextStyle(color: Colors.white)),
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),
+              ),
+            ),
+          ]),
+        ));
   }
 }
