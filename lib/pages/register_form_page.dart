b48dc470ba4eb30dbdf2f59115e151635cdfaf39
Jean <acalto2@gmail.com>
2023-01-29 22:37:17 +0100

    48

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -21,37 +21,95 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
               decoration: InputDecoration(
                 labelText: 'Full name *',
                 hintText: 'Enter your name',
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(Icons.delete_outline, color: Colors.red),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      20,
+                    ),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      20,
+                    ),
+                  ),
+                  borderSide: BorderSide(color: Colors.blue, width: 2),
+                ),
               ),
             ),
             SizedBox(height: 16),
             TextFormField(
               decoration: InputDecoration(
                 labelText: 'Phone Number *',
-                hintText: 'Enter your email',
+                hintText: 'Enter your phone',
+                helperText: 'Phone format: (XXX)XXX-XXXX',
+                prefixIcon: Icon(Icons.phone),
+                suffixIcon: Icon(Icons.delete_outline, color: Colors.red),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      20,
+                    ),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      20,
+                    ),
+                  ),
+                  borderSide: BorderSide(color: Colors.blue, width: 2),
+                ),
               ),
             ),
             TextFormField(
               decoration: InputDecoration(
                 labelText: 'Email *',
                 hintText: 'Enter your email',
+                icon: Icon(Icons.email),
               ),
             ),
             TextFormField(
               decoration: InputDecoration(
                 labelText: 'Life Story',
                 hintText: 'Enter your story',
+                helperText: 'Keep it short, this is just a demo.',
+                border: OutlineInputBorder(),
               ),
+              maxLines: 3,
             ),
             TextFormField(
               decoration: InputDecoration(
                 labelText: 'Password *',
                 hintText: 'Enter your password',
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.visibility),
+                ),
+                icon: Icon(Icons.security),
               ),
             ),
             TextFormField(
               decoration: InputDecoration(
                 labelText: 'Confirm Password *',
                 hintText: 'Confirm your password',
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.visibility),
+                ),
+                icon: Icon(Icons.border_color),
+              ),
               ),
             ),
             SizedBox(height: 16),
