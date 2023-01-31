4fd78edd446e1e9dfbf52cabb14f3f636f98cf51
Jean <acalto2@gmail.com>
2023-01-31 18:56:41 +0100

    67

--- lib/offices.dart
+++ lib/offices.dart
@@ -1,20 +1,22 @@
 import 'dart:convert';
 
+import 'package:json_annotation/json_annotation.dart';
+
 import 'package:http/http.dart' as http;
 
+part 'offices.g.dart';
 
+@JsonSerializable()
 class OfficesList {
   List<Office> offices;
   OfficesList({required this.offices});
 
-  factory OfficesList.fromJson(Map<String, dynamic> json) {
-    var officesJson = json['offices'] as List;
-    List<Office> officesList =
-        officesJson.map((i) => Office.fromJson(i)).toList();
-    return OfficesList(offices: officesList);
-  }
+  factory OfficesList.fromJson(Map<String, dynamic> json) =>
+      _$OfficesListFromJson(json);
+      Map<String, dynamic> toJson() => _$OfficesListToJson(this);
 }
 
+@JsonSerializable()
 class Office {
   final String name;
   final String address;
@@ -22,13 +24,9 @@ class Office {
 
   Office({required this.name, required this.address, required this.image});
 
-  factory Office.fromJson(Map<String, dynamic> json) {
-    return Office(
-      name: json['name'] as String,
-      address: json['address'] as String,
-      image: json['image'] as String,
-    );
-  }
+  factory Office.fromJson(Map<String, dynamic> json) => _$OfficeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OfficeToJson(this);
 }
 
 Future<OfficesList> getOfficesList() async {
@@ -40,5 +38,4 @@ Future<OfficesList> getOfficesList() async {
   } else {
     throw Exception('Error: ${response.reasonPhrase}');
   }
-
 }
