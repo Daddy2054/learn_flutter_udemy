de6b7233b91d5ba0226baf6ad2569a2dd54cde8a
Jean <acalto2@gmail.com>
2023-02-01 22:32:53 +0100

    71

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Inherited Demo',
+      title: 'ScopedModel Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Inherited Demo'),
+      home: const MyHomePage(title: 'ScopedModel Demo'),
     );
   }
 }
@@ -23,29 +24,22 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  MyHomePageState createState() => MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  int get counterValue => _counter;
-
-  void _incrementCounter() => setState(() => _counter++);
-  void _decrementCounter() => setState(() => _counter--);
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Inherited Widget'),
+        title: const Text('ScopedModel Widget'),
         centerTitle: true,
       ),
       body: ListView(
         children: <Widget>[
-          MyInheritedWidget(
-            myState: this,
-            child: const AppRootWidget(),
+          ScopedModel<MyModelState>(
+            model: MyModelState(),
+            child: _AppRootWidget(),
           ),
         ],
       ),
@@ -53,25 +47,20 @@ class MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class AppRootWidget extends StatelessWidget {
-  const AppRootWidget({Key? key}) : super(key: key);
-
+class _AppRootWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final rootWidgetState = MyInheritedWidget.of(context)!.myState;
     return Card(
       elevation: 4.0,
       child: Column(
         children: <Widget>[
           Text('(Root Widget)', style: Theme.of(context).textTheme.headline4),
-          Text('${rootWidgetState.counterValue}',
-              style: Theme.of(context).textTheme.headline4),
           const SizedBox(height: 50),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const <Widget>[
-              Counter(),
-              Counter(),
+            children: <Widget>[
+              _Counter(),
+              _Counter(),
             ],
           ),
         ],
@@ -80,54 +69,49 @@ class AppRootWidget extends StatelessWidget {
   }
 }
 
-class Counter extends StatelessWidget {
-  const Counter({Key? key}) : super(key: key);
-
+class _Counter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final rootWidgetState = MyInheritedWidget.of(context)!.myState;
-    return Card(
-      margin: const EdgeInsets.all(4.0).copyWith(bottom: 32.0),
-      color: Colors.yellowAccent,
-      child: Column(
-        children: <Widget>[
-          const Text('(Child Widget)'),
-          Text('${rootWidgetState.counterValue}',
-              style: Theme.of(context).textTheme.headline4),
-          ButtonBar(
-            children: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.remove),
-                color: Colors.red,
-                onPressed: () => rootWidgetState._decrementCounter(),
+    return ScopedModelDescendant<MyModelState>(
+        rebuildOnChange: true,
+        builder: (context, child, model) => Card(
+              margin: const EdgeInsets.all(4.0).copyWith(bottom: 32.0),
+              color: Colors.yellowAccent,
+              child: Column(
+                children: <Widget>[
+                  const Text('(Child Widget)'),
+                  Text('${model.counterValue}',
+                      style: Theme.of(context).textTheme.headline4),
+                  ButtonBar(
+                    children: <Widget>[
+                      IconButton(
+                        icon: const Icon(Icons.remove),
+                        color: Colors.red,
+                        onPressed: () => model._decrementCounter(),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.add),
+                        color: Colors.green,
+                        onPressed: () => model._incrementCounter(),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              IconButton(
-                icon: const Icon(Icons.add),
-                color: Colors.green,
-                onPressed: () => rootWidgetState._incrementCounter(),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+            ));
   }
 }
 
-class MyInheritedWidget extends InheritedWidget {
-  final MyHomePageState myState;
-
-  const MyInheritedWidget(
-      {Key? key, required Widget child, required this.myState})
-      : super(key: key, child: child);
-
-  @override
-  bool updateShouldNotify(MyInheritedWidget oldWidget) {
-    print(myState.counterValue);
-    return myState.counterValue != oldWidget.myState.counterValue;
+class MyModelState extends Model {
+  int _counter = 0;
+  int get counterValue => _counter;
+  void _incrementCounter() {
+    _counter++;
+    notifyListeners();
   }
 
-  static MyInheritedWidget? of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType();
+  void _decrementCounter() {
+    _counter--;
+    notifyListeners();
   }
 }
