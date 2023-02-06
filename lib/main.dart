577304f95cd3239a298d65b52b9783de61fd15ee
Jean <acalto2@gmail.com>
2023-02-06 23:58:28 +0100

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,215 +1,115 @@
 import 'package:flutter/material.dart';
-import 'package:mysample/db/database.dart';
-import 'package:mysample/model/student.dart';
 
-
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SQLite CRUD Demo',
-      home: StudentPage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class StudentPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  _StudentPageState createState() => _StudentPageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _StudentPageState extends State<StudentPage> {
-  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
-  final _studentNameController = TextEditingController();
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  late Future<List<Student>> _studentsList;
-  late String _studentName;
-  bool isUpdate = false;
-  int? studentIdForUpdate;
-
-  @override
-  void initState() {
-    super.initState();
-    updateStudentList();
-  }
-
-  updateStudentList() {
+  void _incrementCounter() {
     setState(() {
-      _studentsList = DBProvider.db.getStudents();
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text('SQLite CRUD Demo'),
-        centerTitle: true,
-        backgroundColor: Colors.black,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Form(
-            key: _formStateKey,
-            autovalidateMode: AutovalidateMode.always, 
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value == null) {
-                        return 'Please Enter Student Name';
-                      }
-                      if (value.trim() == "")
-                        return "Only Space is Not Valid!!!";
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _studentName = value!;
-                    },
-                    controller: _studentNameController,
-                    decoration: InputDecoration(
-                      focusedBorder: new UnderlineInputBorder(
-                        borderSide: new BorderSide(
-                            color: Colors.greenAccent,
-                            width: 2,
-                            style: BorderStyle.solid),
-                      ),
-                      labelText: "Student Name",
-                      icon: Icon(
-                        Icons.people,
-                        color: Colors.black,
-                      ),
-                      fillColor: Colors.white,
-                      labelStyle: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.green,
-                  textStyle: TextStyle(color: Colors.white),
-                ),
-                child: Text(
-                  (isUpdate ? 'UPDATE' : 'ADD'),
-                ),
-                onPressed: () {
-                  if (isUpdate) {
-                    if (_formStateKey.currentState!.validate()) {
-                      _formStateKey.currentState!.save();
-                      DBProvider.db
-                          .updateStudent(
-                              Student(studentIdForUpdate!, _studentName))
-                          .then((data) {
-                        setState(() {
-                          isUpdate = false;
-                        });
-                      });
-                    }
-                  } else {
-                    if (_formStateKey.currentState!.validate()) {
-                      _formStateKey.currentState!.save();
-                      // DBProvider.db.insertStudent(Student(null, _studentName));
-                      DBProvider.db.insertStudent(Student(null, _studentName));
-                    }
-                  }
-                  _studentNameController.text = '';
-                  updateStudentList();
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.red,
-                  textStyle: TextStyle(color: Colors.white),
-                ),
-                child: Text(
-                  (isUpdate ? 'CANCEL UPDATE' : 'CLEAR'),
-                ),
-                onPressed: () {
-                  _studentNameController.text = '';
-                  setState(() {
-                    isUpdate = false;
-                    studentIdForUpdate = null; // null;
-                  });
-                },
-              ),
-            ],
-          ),
-          const Divider(
-            height: 5.0,
-          ),
-          Expanded(
-            child: FutureBuilder(
-              future: _studentsList,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return generateList(snapshot.data as List<Student>);
-                }
-                if (snapshot.data == null || (snapshot.data as List<Student>).length == 0) {
-                  return Text('No Data Found');
-                }
-                return CircularProgressIndicator();
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  SingleChildScrollView generateList(List<Student> students) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: DataTable(
-          columns: [
-            DataColumn(
-              label: Text('NAME'),
-            ),
-            DataColumn(
-              label: Text('DELETE'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
-          rows: students
-              .map(
-                (student) => DataRow(cells: [
-                  DataCell(Text(student.name), onTap: () {
-                    setState(() {
-                      isUpdate = true;
-                      studentIdForUpdate = student.id;
-                    });
-                    _studentNameController.text = student.name;
-                  }),
-                  DataCell(
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        DBProvider.db.deleteStudent(student.id);
-                        updateStudentList();
-                      },
-                    ),
-                  ),
-                ]),
-              )
-              .toList(),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
